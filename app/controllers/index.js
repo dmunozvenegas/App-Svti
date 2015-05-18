da7e125ca9cc4062a6a9d2ce771713212d033531
Diego Muñoz <diegomunoz@MacBook-Air-de-Diego.local>
2015-05-18 18:06:12 -0400

    svti

---
+++ app/controllers/index.js
@@ -0,0 +1,51 @@
+function doClick(e) {
+
+	getBooking($.booking.value, function(items){
+		Ti.API.info(items);
+		var data = [i];
+		for (var i=0;i<items.length;i++) {
+			    data.push({
+					descripcion: items.item(i).getElementsByTagName("descripcion").item(0).textContent,
+					estado_requerido: items.item(i).getElementsByTagName("estado_requerido").item(0).textContent,
+					estado_tramitado: items.item(i).getElementsByTagName("estado_tramitado").item(0).textContent
+			});
+		}
+			
+	 		$.descripcion.text = JSON.stringify(data[i].descripcion);
+	 		$.estado_requerido.text = JSON.stringify(data[i].estado_requerido);
+	 		$.estado_tramitado.text = JSON.stringify(data[i].estado_tramitado);
+	 		
+		
+	});
+}
+
+function getBooking(busqueda, callback){
+	var xhr = Titanium.Network.createHTTPClient();
+	var data = [];
+	// Si el servicio web no responde en 10 segundos nos dará error de Timeout.
+	xhr.setTimeout(10000);
+	var url =  "http://svtiweb.svti.cl:7001/wsmovil/sve_ws_cexpo.asmx/wsm_consulta_reserva?res_numero="+busqueda;
+	// Hacemos el GET al servicio web.
+	xhr.open("GET",url);
+ 
+	xhr.onload = function() {
+		//Los datos se devuelve, inicia analisis
+		var doc = this.responseXML.documentElement;
+		//Comienza a recorrer el arreglo
+		var items = doc.getElementsByTagName("Item_consulta_reserva");
+		callback(items);
+		
+	};
+	//Gestiona en caso de error
+	xhr.onerror = function(e){
+		var a = Ti.UI.createAlertDialog();
+	 	a.message='Error en conexión';
+	 	a.title='Atencion';
+	 	a.show();
+	 	callback('-1');
+	};
+	
+	xhr.send();
+}
+
+$.index.open();
